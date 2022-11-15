d930d0551ff75132a8e1a671fb109e9f0e5c066e
albyschianni <alberto.schianni@gmail.com>
2022-11-15 18:44:36 +0100

    update pari e dispari

--- PARI E DISPARI/js/main.js
+++ PARI E DISPARI/js/main.js
@@ -3,3 +3,48 @@
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+
+let pariODispari = prompt("Scegli pari o dispari");
+let numero = parseInt(prompt("Scegli un numero"));
+
+console.log(pariODispari);
+console.log(numero);
+
+let pcNumber = randomPcNumber(1, 5);
+
+function randomPcNumber (min, max) {
+
+    let numerogenerato = Math.floor(Math.random() * (max - min + 1)) + min;
+    return numerogenerato;
+    
+}
+console.log("pc number " + pcNumber);
+
+
+const somma = sommaNumeri(numero, pcNumber);
+
+ValoreRisultato();
+
+function sommaNumeri (num1, num2) {
+
+    const somma = num1 + num2;
+    
+    const risultato = [somma];
+
+    return risultato;
+
+    
+}
+console.log("la somma è " + somma);
+
+
+function ValoreRisultato () {
+        
+    if (somma % 2 === 0){
+        alert("vince pari");
+    } else if (somma % 2 === 1) {
+        alert("vince dispari");
+    }
+    
+}
